39df33224860dc79948a47f468382b0b430844d9
Prysmytskyi-Oleksandr <108235067+Prysmytskyi-Oleksandr@users.noreply.github.com>
2022-12-02 08:58:53 +0000

    Deploying to gh-pages from @ Prysmytskyi-Oleksandr/goit-js-hw-09@11d96228f9f4da4af223a4d2fb0b1e481a1bdbe7 🚀

---
+++ 01-color-switcher.e0f22877.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(t){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.target.setAttribute("disabled","true"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(e){e.target.setAttribute("disabled","true"),t.removeAttribute("disabled"),clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.e0f22877.js.map
